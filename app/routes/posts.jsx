54b30abf449dc398e2e78bcf027735d9ad9c7d27
Leo Huynh <huynhtuananh21895@gmail.com>
2021-12-20 09:22:53 +0700

    File based routing

---
+++ app/routes/posts.jsx
@@ -0,0 +1,7 @@
+import { Outlet } from "remix"
+
+function Posts() {
+  return <Outlet />
+}
+
+export default Posts
---
+++ app/routes/posts/new.jsx
@@ -0,0 +1,46 @@
+import { Link, redirect } from "remix"
+import { db } from "~/utils/db.server"
+
+export const action = async ({ request }) => {
+  const form = await request.formData()
+  const title = form.get('title')
+  const body = form.get('body')
+
+  const post = await db.post.create({
+    data: {
+      title, body
+    }
+  })
+
+  return redirect(`/posts/${post.id}`)
+}
+
+function NewPost() {
+  return (
+    <>
+      <div className="page-header">
+        <h1>New post</h1>
+        <Link to="/posts" className="btn btn-reverse">
+          Back
+        </Link>
+      </div>
+      <div className="page-content">
+        <form method="POST">
+          <div className="form-control">
+            <label htmlFor="title">Title</label>
+            <input type="text" name="title" id="title" />
+          </div>
+          <div className="form-control">
+            <label htmlFor="body">Post body</label>
+            <textarea name="body" id="body" cols="30" rows="10"></textarea>
+          </div>
+          <button type="submit" className="btn btn-block">
+            Add post
+          </button>
+        </form>
+      </div>
+    </>
+  )
+}
+
+export default NewPost
